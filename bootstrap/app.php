0b769fc7f47a56b57608cd53627374e206d11911
Fabrizio Machado <fabrizio.machado@gmail.com>
2026-04-18 19:12:53 -0300

    fix(http): confiar em proxies para HTTPS e URLs corretas atrás de load balancer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
